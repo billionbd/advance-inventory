5813cdea12761ca3d24208337ba23da9f16d8e91
Viet Tung <user@users-MacBook-Pro.local>
2018-06-04 23:07:54 +0700

    Commit source to git

---
+++ advance_inventory/application/views/templates/side_menubar.php
@@ -0,0 +1,26 @@
+
+<aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+      
+      <!-- sidebar menu: : style can be found in sidebar.less -->
+      <ul class="sidebar-menu" data-widget="tree">
+        
+        <li id="dashboardMainMenu">
+          <a href="<?php echo base_url('dashboard') ?>">
+            <i class="fa fa-dashboard"></i> <span>Dashboard</span>
+          </a>
+        </li>
+        <li id="brandNav">
+              <a href="<?php echo base_url('Controller_transaction/') ?>">
+                <i class="fa fa-cart-arrow-down"></i> <span>Transaction</span>
+              </a>
+            </li>
+
+        <!-- user permission info -->
+        <li><a href="<?php echo base_url('auth/logout') ?>"><i class="glyphicon glyphicon-log-out"></i> <span>Logout</span></a></li>
+
+      </ul>
+    </section>
+    <!-- /.sidebar -->
+  </aside>
